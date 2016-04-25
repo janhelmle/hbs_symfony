3d256f57ca9c5c49818a66f1f90174bdc7f24e88
Jan Helmle <jhelmle@gmx.de>
2016-04-25 17:48:25 +0200

    Fixture LoadRoomTypeData hinzugefuegt

---
+++ src/AppBundle/DataFixtures/ORM/LoadRoomTypeData.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\RoomType;
+
+class LoadRoomTypeData implements FixtureInterface {
+
+    public function load(ObjectManager $manager) {
+
+        $rt1 = new RoomType();
+        $rt1->setIdentifier("singleroom");
+        $rt1->setDisplayShort("Einzel");
+        $rt1->setDisplayLong("Einzelzimmer");
+        $rt1->setPricingBasis("night");
+        $rt1->setPricingBasisDisplay("/Nacht");
+        $rt1->setCapacity(5);
+        $rt1->setQuantityOfPersons(1);
+        $rt1->setPositionInSubMenu(1);
+        $manager->persist($rt1);
+        $manager->flush();
+        unset($rt1);
+
+        $rt2 = new RoomType();
+        $rt2->setIdentifier("doubleroom");
+        $rt2->setDisplayShort("Doppel");
+        $rt2->setDisplayLong("Doppelzimmer");
+        $rt2->setPricingBasis("night");
+        $rt2->setPricingBasisDisplay("/Nacht");
+        $rt2->setCapacity(5);
+        $rt2->setQuantityOfPersons(2);
+        $rt2->setPositionInSubMenu(2);
+        $manager->persist($rt2);
+        $manager->flush();
+        unset($rt2);
+
+        $rt3 = new RoomType();
+        $rt3->setIdentifier("twinroom");
+        $rt3->setDisplayShort("Zweibett");
+        $rt3->setDisplayLong("Zweibettzimmer");
+        $rt3->setPricingBasis("night");
+        $rt3->setPricingBasisDisplay("/Nacht");
+        $rt3->setCapacity(5);
+        $rt3->setQuantityOfPersons(2);
+        $rt3->setPositionInSubMenu(3);
+        $manager->persist($rt3);
+        $manager->flush();
+        unset($rt3);
+
+        $rt4 = new RoomType();
+        $rt4->setIdentifier("tripleroom");
+        $rt4->setDisplayShort("Dreibett");
+        $rt4->setDisplayLong("Dreibettzimmer");
+        $rt4->setPricingBasis("night");
+        $rt4->setPricingBasisDisplay("/Nacht");
+        $rt4->setCapacity(5);
+        $rt4->setQuantityOfPersons(3);
+        $rt4->setPositionInSubMenu(4);
+        $manager->persist($rt4);
+        $manager->flush();
+        unset($rt4);
+
+        $rt5 = new RoomType();
+        $rt5->setIdentifier("familyroom");
+        $rt5->setDisplayShort("Familie");
+        $rt5->setDisplayLong("Familien-/Vierbettzimmer");
+        $rt5->setPricingBasis("night");
+        $rt5->setPricingBasisDisplay("/Nacht");
+        $rt5->setCapacity(5);
+        $rt5->setQuantityOfPersons(4);
+        $rt5->setPositionInSubMenu(5);
+        $manager->persist($rt5);
+        $manager->flush();
+        unset($rt5);
+
+        $rt6 = new RoomType();
+        $rt6->setIdentifier("apartmentsingle");
+        $rt6->setDisplayShort("Apartm. EZ");
+        $rt6->setDisplayLong("Apartment als Einzelzimmer");
+        $rt6->setPricingBasis("night");
+        $rt6->setPricingBasisDisplay("/Nacht");
+        $rt6->setCapacity(5);
+        $rt6->setQuantityOfPersons(1);
+        $rt6->setPositionInSubMenu(6);
+        $manager->persist($rt6);
+        $manager->flush();
+        unset($rt6);
+
+        $rt7 = new RoomType();
+        $rt7->setIdentifier("apartmentdouble");
+        $rt7->setDisplayShort("Apartm. DZ");
+        $rt7->setDisplayLong("Apartment als Doppelzimmer");
+        $rt7->setPricingBasis("night");
+        $rt7->setPricingBasisDisplay("/Nacht");
+        $rt7->setCapacity(5);
+        $rt7->setQuantityOfPersons(2);
+        $rt7->setPositionInSubMenu(7);
+        $manager->persist($rt7);
+        $manager->flush();
+        unset($rt7);
+    }
+
+}
