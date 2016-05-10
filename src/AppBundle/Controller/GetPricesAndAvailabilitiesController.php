c745f384af14e70b5509386577786a18ef76ac3e
Jan Helmle <jhelmle@gmx.de>
2016-05-10 13:31:42 +0200

    Controller getPricesAndAvailabilitiesAction v0.2 hinzugefuegt

--- src/AppBundle/Controller/GetPricesAndAvailabilitiesController.php
+++ src/AppBundle/Controller/GetPricesAndAvailabilitiesController.php
@@ -14,6 +14,127 @@ use stdClass;
 use DateTime;
 
 class GetPricesAndAvailabilitiesController extends Controller {
+    
+    /**
+     * @Route("/api/v0.2/getpricesandavailabilities", name="getpricesandavailabilities_v_0.2")
+     */
+    public function getPricesAndAvailabilitiesAction_v_02(Request $request) {
+        
+        $em = $this->getDoctrine()->getManager();
+
+        $checkInDate = $request->headers->get('checkInDate'); // 2016.04.26, 12:00
+        $checkOutDate = $request->headers->get('checkOutDate'); // 2016.04.27, 12:00
+
+        $checkInDateTime = DateTime::createFromFormat('Y.m.d, H:i', $checkInDate);
+        $checkOutDateTime = DateTime::createFromFormat('Y.m.d, H:i', $checkOutDate);
+        
+        if (
+                (!$checkInDateTime) OR (!$checkOutDateTime)
+        ) {
+
+
+            $resp = new Response(
+                    "malformed request syntax. "
+                    . "Please use header keys 'checkInDate' and 'checkOutDate' with values in this form : "
+                    . "'Y.m.d, H:i' , e.g. '2016.04.26, 12:00'"
+            );
+            $resp->setStatusCode(Response::HTTP_BAD_REQUEST);
+            $resp->headers->set('Content-Type', 'Content-Type: text/html; charset=utf-8');
+            return $resp;
+        }
+        
+        if (
+                $checkInDateTime >= $checkOutDateTime 
+        ) {
+
+
+            $resp = new Response(
+                    "ERROR: checkInDate >= checkOutDate"
+            );
+            $resp->setStatusCode(Response::HTTP_BAD_REQUEST);
+            $resp->headers->set('Content-Type', 'Content-Type: text/html; charset=utf-8');
+            return $resp;
+        }
+
+        $dto = new stdClass();
+
+        $dto->checkInDate = $checkInDate;
+        $dto->checkOutDate = $checkOutDate;
+
+
+        $query = $em->createQuery('
+            
+                SELECT 
+                    rt
+                FROM
+                    AppBundle:RoomType rt
+                    
+                ');
+
+        $roomTypes = $query->getResult();
+        
+        foreach($roomTypes as $rt) {
+            $i = new stdClass();
+            $i->identifier = $rt->getIdentifier();
+            $i->price = $rt->getPrices()[0]->getValue();
+            $i->quantity = $rt->getAvailabilities()[0]->getQuantity();
+            $dto->roomTypes[] = $i;
+        }
+        
+        $query = $em->createQuery("
+            
+                SELECT 
+                    ap
+                FROM
+                    AppBundle:AdditionalProduct ap
+                JOIN
+                    ap.additionalproductcategory apc
+                WHERE
+                    apc.identifier = 'boardings'
+                    
+                ");
+
+        $additionalProducts = $query->getResult();
+        
+        foreach($additionalProducts as $ap) {
+            $i = new stdClass();
+            $i->identifier = $ap->getIdentifier();
+            $i->price = $ap->getPrices()[0]->getValue();
+            $dto->boardings[] = $i;
+        }
+        
+        $query = $em->createQuery("
+            
+                SELECT 
+                    ap
+                FROM
+                    AppBundle:AdditionalProduct ap
+                JOIN
+                    ap.additionalproductcategory apc
+                WHERE
+                    apc.identifier = 'specials'
+                    
+                ");
+
+        $additionalProducts = $query->getResult();
+        
+        foreach($additionalProducts as $ap) {
+            $i = new stdClass();
+            $i->identifier = $ap->getIdentifier();
+            $i->price = $ap->getPrices()[0]->getValue();
+            $dto->specials[] = $i;
+        }
+        
+        $pandaJSON = json_encode($dto, 320); // 320 : 0000000101000000 = 256 + 64 : JSON_UNESCAPED_SLASHES => 64 + JSON_UNESCAPED_UNICODE => 256
+
+        $resp = new Response($pandaJSON);
+        $resp->headers->set('Content-Type', 'application/json ; charset=utf-8');
+
+        return $resp;
+    }
+
+    
+    
 
     /**
      * @Route("/api/v0.1/getpricesandavailabilities", name="getpricesandavailabilities_v_0.1")
