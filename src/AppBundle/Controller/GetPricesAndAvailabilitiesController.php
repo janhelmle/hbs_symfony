47b2597c37d2c61b6374949662b7ba2c572da6ce
Jan Helmle <jhelmle@gmx.de>
2016-06-02 20:30:58 +0200

    Ueberfluessige use statements in GetPricesAndAvailabilitiesController entfernt

--- src/AppBundle/Controller/GetPricesAndAvailabilitiesController.php
+++ src/AppBundle/Controller/GetPricesAndAvailabilitiesController.php
@@ -6,10 +6,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use AppBundle\Entity\Product;
-use AppBundle\Entity\RoomType;
-use AppBundle\Entity\AdditionalProduct;
-use AppBundle\Entity\AdditionalProductCategory;
 use stdClass;
 use DateTime;
 
