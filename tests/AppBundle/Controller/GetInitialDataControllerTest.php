e7b70d8fcd89052a83dc5d96883c6895523e885e
Jan Helmle <jhelmle@gmx.de>
2016-06-06 16:30:20 +0200

    GetInitialDataControllerTest: JSON Content Assertion hinzugefuegt

--- tests/AppBundle/Controller/GetInitialDataControllerTest.php
+++ tests/AppBundle/Controller/GetInitialDataControllerTest.php
@@ -31,12 +31,96 @@ class GetInitialDataTest extends WebTestCase {
         );
     }
 
-    public function testNotEmpty_v0() {
+    public function testContentNotEmpty_v0() {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/api/v0/getinitialdata');
 
         $this->assertNotEmpty($client->getResponse()->getContent());
+    }
+
+    public function testJsonContent_v0() {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/api/v0/getinitialdata');
+
+        $expected = '{
+	"roomtypes": [{
+		"identifier": "singleroom",
+		"subMenuText": "Einzel",
+		"listText": "Einzelzimmer",
+		"pricingBasisText": "/Nacht",
+		"capacity": 5
+	}, {
+		"identifier": "doubleroom",
+		"subMenuText": "Doppel",
+		"listText": "Doppelzimmer",
+		"pricingBasisText": "/Nacht",
+		"capacity": 6
+	}, {
+		"identifier": "twinroom",
+		"subMenuText": "Zweibett",
+		"listText": "Zweibettzimmer",
+		"pricingBasisText": "/Nacht",
+		"capacity": 7
+	}, {
+		"identifier": "tripleroom",
+		"subMenuText": "Dreibett",
+		"listText": "Dreibettzimmer",
+		"pricingBasisText": "/Nacht",
+		"capacity": 8
+	}, {
+		"identifier": "familyroom",
+		"subMenuText": "Familie",
+		"listText": "Familien-/Vierbettzimmer",
+		"pricingBasisText": "/Nacht",
+		"capacity": 4
+	}, {
+		"identifier": "apartmentsingle",
+		"subMenuText": "Apartm. EZ",
+		"listText": "Apartment als Einzelzimmer",
+		"pricingBasisText": "/Nacht",
+		"capacity": 3
+	}, {
+		"identifier": "apartmentdouble",
+		"subMenuText": "Apartm. DZ",
+		"listText": "Apartment als Doppelzimmer",
+		"pricingBasisText": "/Nacht",
+		"capacity": 2
+	}],
+	"boardings": [{
+		"identifier": "halfpension",
+		"listText": "Halbpension (mit Frühstück)",
+		"pricingBasisText": "/Pers. u. Nacht"
+	}, {
+		"identifier": "fullpension",
+		"listText": "Vollpension (3 Mahlzeiten)",
+		"pricingBasisText": "/Pers. u. Nacht"
+	}, {
+		"identifier": "breakfast",
+		"listText": "Nur Frühstück",
+		"pricingBasisText": "/Pers. u. Nacht"
+	}, {
+		"identifier": "noboarding",
+		"listText": "Ohne Verpflegung",
+		"pricingBasisText": ""
+	}],
+	"specials": [{
+		"identifier": "champagnebreakfast",
+		"listText": "Sektfrühstück",
+		"pricingBasisText": "/Pers. u. Nacht"
+	}, {
+		"identifier": "rosesinrooms",
+		"listText": "Rosen auf das Zimmer",
+		"pricingBasisText": ""
+	}, {
+		"identifier": "raftingtour",
+		"listText": "Rafting-Tour",
+		"pricingBasisText": "/Person"
+	}]
+        }';
+
+        $this->assertJsonStringEqualsJsonString($client->getResponse()->getContent(), $expected);
     }
 
     //  /api/v0.1/getinitialdata //
@@ -63,13 +147,102 @@ class GetInitialDataTest extends WebTestCase {
                 $client->getResponse()->getStatusCode()
         );
     }
-    
-    public function testNotEmpty_v0_1() {
+
+    public function testContentNotEmpty_v0_1() {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/api/v0.1/getinitialdata');
 
         $this->assertNotEmpty($client->getResponse()->getContent());
+    }
+
+    public function testJsonContent_v0_1() {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/api/v0.1/getinitialdata');
+
+        $expected = '{
+	"roomTypes": {
+		"singleroom": {
+			"subMenuText": "Einzel",
+			"listText": "Einzelzimmer",
+			"pricingBasisText": "/Nacht",
+			"capacity": 5
+		},
+		"doubleroom": {
+			"subMenuText": "Doppel",
+			"listText": "Doppelzimmer",
+			"pricingBasisText": "/Nacht",
+			"capacity": 6
+		},
+		"twinroom": {
+			"subMenuText": "Zweibett",
+			"listText": "Zweibettzimmer",
+			"pricingBasisText": "/Nacht",
+			"capacity": 7
+		},
+		"tripleroom": {
+			"subMenuText": "Dreibett",
+			"listText": "Dreibettzimmer",
+			"pricingBasisText": "/Nacht",
+			"capacity": 8
+		},
+		"familyroom": {
+			"subMenuText": "Familie",
+			"listText": "Familien-/Vierbettzimmer",
+			"pricingBasisText": "/Nacht",
+			"capacity": 4
+		},
+		"apartmentsingle": {
+			"subMenuText": "Apartm. EZ",
+			"listText": "Apartment als Einzelzimmer",
+			"pricingBasisText": "/Nacht",
+			"capacity": 3
+		},
+		"apartmentdouble": {
+			"subMenuText": "Apartm. DZ",
+			"listText": "Apartment als Doppelzimmer",
+			"pricingBasisText": "/Nacht",
+			"capacity": 2
+		}
+	},
+	"boardings": {
+		"subMenuText": "Verpflegung",
+		"halfpension": {
+			"listText": "Halbpension (mit Frühstück)",
+			"pricingBasisText": "/Pers. u. Nacht"
+		},
+		"fullpension": {
+			"listText": "Vollpension (3 Mahlzeiten)",
+			"pricingBasisText": "/Pers. u. Nacht"
+		},
+		"breakfast": {
+			"listText": "Nur Frühstück",
+			"pricingBasisText": "/Pers. u. Nacht"
+		},
+		"noboarding": {
+			"listText": "Ohne Verpflegung",
+			"pricingBasisText": ""
+		}
+	},
+	"specials": {
+		"subMenuText": "Specials (optional)",
+		"champagnebreakfast": {
+			"listText": "Sektfrühstück",
+			"pricingBasisText": "/Pers. u. Nacht"
+		},
+		"rosesinrooms": {
+			"listText": "Rosen auf das Zimmer",
+			"pricingBasisText": ""
+		},
+		"raftingtour": {
+			"listText": "Rafting-Tour",
+			"pricingBasisText": "/Person"
+		}
+	}
+}';
+
+        $this->assertJsonStringEqualsJsonString($client->getResponse()->getContent(), $expected);
     }
 
 }
