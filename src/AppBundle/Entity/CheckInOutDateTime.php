381af073250974309084fd546c149b8c223737ea
Jan Helmle <jhelmle@gmx.de>
2016-06-14 14:57:47 +0200

    Klasse CheckInOutDateTime hinzugefuegt

---
+++ src/AppBundle/Entity/CheckInOutDateTime.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Exception;
+use DateTime;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="checkInOutDateTime")
+ */
+class CheckInOutDateTime {
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id; // integer
+
+    /**
+     * @Assert\DateTime()
+     * @ORM\Column(type="datetime" , nullable=false)
+     */
+    private $checkInDateTime;
+
+    /**
+     * @Assert\DateTime()
+     * @ORM\Column(type="datetime" , nullable=false)
+     */
+    private $checkOutDateTime;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Set checkInDateTime
+     *
+     * @param \DateTime $checkInDateTime
+     *
+     * @return CheckInOutDateTime
+     */
+    public function setCheckInDateTime($checkInDateTime) {
+        $this->checkInDateTime = $checkInDateTime;
+
+        return $this;
+    }
+
+    /**
+     * Get checkInDateTime
+     *
+     * @return \DateTime
+     */
+    public function getCheckInDateTime() {
+        return $this->checkInDateTime;
+    }
+
+    /**
+     * Set checkOutDateTime
+     *
+     * @param \DateTime $checkOutDateTime
+     *
+     * @return CheckInOutDateTime
+     */
+    public function setCheckOutDateTime($checkOutDateTime) {
+        $this->checkOutDateTime = $checkOutDateTime;
+
+        return $this;
+    }
+
+    /**
+     * Get checkOutDateTime
+     *
+     * @return \DateTime
+     */
+    public function getCheckOutDateTime() {
+        return $this->checkOutDateTime;
+    }
+
+    /**
+     * Set checkInOutDateTime
+     *
+     * @param string $checkInDateTime
+     *
+     * @return CheckInOutDateTime
+     */
+    public function setCheckInOutDateTime($checkInDateTime, $checkOutDateTime) {
+        $checkInDateTimeTrimmed = trim($checkInDateTime);
+        $checkOutDateTimeTrimmed = trim($checkOutDateTime);
+
+        if (
+                !(preg_match('/^(19|20)\d{2}\.(0|1)\d\.[0-3]\d,\s[0-2]\d:[0-5]\d$/', $checkInDateTimeTrimmed)) || // test
+                !(preg_match('/^(19|20)\d{2}\.(0|1)\d\.[0-3]\d,\s[0-2]\d:[0-5]\d$/', $checkOutDateTimeTrimmed))
+        ) {
+            throw new Exception("Error. Malformed request syntax. Please use header keys 'checkInDate' and 'checkOutDate' with values in this form : 'Y.m.d, H:i' , e.g. '2016.04.26, 12:00'");
+        }
+
+        $this->checkInDateTime = DateTime::createFromFormat('Y.m.d, H:i', $checkInDateTimeTrimmed);
+        $this->checkOutDateTime = DateTime::createFromFormat('Y.m.d, H:i', $checkOutDateTimeTrimmed);
+
+        return $this;
+    }
+
+    /**
+     * @Assert\IsTrue(message = "The password cannot match your first name")
+     */
+    public function isCheckInDateTimeLegal() {
+        
+        return true;
+    }
+
+    /**
+     * @Assert\IsTrue(message = "Error: Checkout time earlier than Checkin time")
+     */
+    public function isCheckOutDateTimeLaterCheckInDateTime() {
+        if( ($this->getCheckInDateTime()->diff($this->getCheckOutDateTime())->invert) > 0 ) { return false ; }
+        return true;
+    }
+
+}
