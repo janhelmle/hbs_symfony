f9354df09722f3d559522b5b373f4ace1284ce43
Jan Helmle <jhelmle@gmx.de>
2016-06-30 16:19:32 +0200

    Controller : GetPricesAndAvailabilitiesController neuen Endpoint erzeugt

--- src/AppBundle/Controller/GetPricesAndAvailabilitiesController.php
+++ src/AppBundle/Controller/GetPricesAndAvailabilitiesController.php
@@ -13,6 +13,75 @@ use AppBundle\Entity\CheckInOutDateTime;
 class GetPricesAndAvailabilitiesController extends Controller {
 
     /**
+     * @Route("/api/v0.3/getpricesandavailabilities", name="getpricesandavailabilities_v_0.3")
+     */
+    public function getPricesAndAvailabilitiesAction_v_03(Request $request) {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $dto = new stdClass(); // new Data Transfer Object
+
+        $checkInDateFromApp = $request->headers->get('checkInDate'); // 2016.04.26, 12:00
+        $checkOutDateFromApp = $request->headers->get('checkOutDate'); // 2016.04.27, 12:00
+
+        // $checkInDateFromApp = '2016.07.26, 12:00';
+        // $checkOutDateFromApp = '2016.07.27, 12:00';
+
+        $ciodt = new CheckInOutDateTime();
+        $ciodt->setCheckInOutDateTime($checkInDateFromApp, $checkOutDateFromApp);
+
+        $validator = $this->get('validator');
+        $errors = $validator->validate($ciodt);
+
+        if (count($errors) > 0) {
+            return (new Response((string) $errors))->setStatusCode(Response::HTTP_BAD_REQUEST); // ->headers->set('Content-Type', 'text/html ; charset=utf-8');
+        }
+
+        $dto->checkInDate = $ciodt->getCheckInDateTime()->format('Y.m.d, H:i');
+        $dto->checkOutDate = $ciodt->getCheckOutDateTime()->format('Y.m.d, H:i');
+
+
+        $roomTypes = $em->getRepository('AppBundle:RoomType')->findAllWhereCapacityGreaterZeroOrderedByPositionInSubMenu(); // array of RoomType objects
+
+        foreach ($roomTypes as $rt) {
+            $i = new stdClass();
+            $i->identifier = $rt->getIdentifier();
+            $i->price = $em->getRepository('AppBundle:Price')
+                    ->calculatePriceAveragePerProductAndDateInterval($rt, $ciodt->getCheckInDateTime(), $ciodt->getCheckOutDateTime());
+            $i->quantity = $em->getRepository('AppBundle:Availability')->findAvailability($ciodt, $rt);
+            $dto->roomTypes[] = $i;
+        }
+
+
+        $boardings = $em->getRepository('AppBundle:AdditionalProduct')->findAllBoardingsOrderedByPositionInList(); // array of AdditionalProduct objects
+
+        foreach ($boardings as $b) {
+            $i = new stdClass();
+            $i->identifier = $b->getIdentifier();
+            $i->price = $em->getRepository('AppBundle:Price')
+                    ->calculatePriceAveragePerProductAndDateInterval($b, $ciodt->getCheckInDateTime(), $ciodt->getCheckOutDateTime());
+            $dto->boardings[] = $i;
+        }
+
+
+        $specials = $em->getRepository('AppBundle:AdditionalProduct')->findAllSpecialsOrderedByPositionInList(); // array of AdditionalProduct objects
+
+        foreach ($specials as $s) {
+            $i = new stdClass();
+            $i->identifier = $s->getIdentifier();
+            $i->price = $em->getRepository('AppBundle:Price')
+                    ->calculatePriceAveragePerProductAndDateInterval($s, $ciodt->getCheckInDateTime(), $ciodt->getCheckOutDateTime());
+            $dto->specials[] = $i;
+        }
+
+        $pandaJSON = json_encode($dto, 320); // 320 : 0000000101000000 = 256 + 64 : JSON_UNESCAPED_SLASHES => 64 + JSON_UNESCAPED_UNICODE => 256
+        $resp = new Response($pandaJSON);
+        $resp->headers->set('Content-Type', 'application/json ; charset=utf-8');
+
+        return $resp;
+    }
+
+    /**
      * @Route("/api/v0.2/getpricesandavailabilities", name="getpricesandavailabilities_v_0.2")
      */
     public function getPricesAndAvailabilitiesAction_v_02(Request $request) {
@@ -148,13 +217,13 @@ class GetPricesAndAvailabilitiesController extends Controller {
 
 
         $query = $em->createQuery('
-            
-                SELECT 
+
+                SELECT
                     rt
                 FROM
                     AppBundle:RoomType rt
                 WHERE rt.enabled = TRUE
-                    
+
                 ');
 
         $roomTypes = $query->getResult();
@@ -168,8 +237,8 @@ class GetPricesAndAvailabilitiesController extends Controller {
         }
 
         $query = $em->createQuery("
-            
-                SELECT 
+
+                SELECT
                     ap
                 FROM
                     AppBundle:AdditionalProduct ap
@@ -178,7 +247,7 @@ class GetPricesAndAvailabilitiesController extends Controller {
                 WHERE
                     apc.identifier = 'boardings'
                 AND ap.enabled = TRUE
-                    
+
                 ");
 
         $additionalProducts = $query->getResult();
@@ -191,8 +260,8 @@ class GetPricesAndAvailabilitiesController extends Controller {
         }
 
         $query = $em->createQuery("
-            
-                SELECT 
+
+                SELECT
                     ap
                 FROM
                     AppBundle:AdditionalProduct ap
@@ -201,7 +270,7 @@ class GetPricesAndAvailabilitiesController extends Controller {
                 WHERE
                     apc.identifier = 'specials'
                 AND ap.enabled = TRUE
-                    
+
                 ");
 
         $additionalProducts = $query->getResult();
@@ -229,55 +298,55 @@ class GetPricesAndAvailabilitiesController extends Controller {
         $em = $this->getDoctrine()->getManager();
 
         $query = $em->createQuery("
-                SELECT 
-                
+                SELECT
+
                     ap.identifier ,
                     MAX(pri.value) AS price
-                
+
                 FROM AppBundle:AdditionalProduct ap
-                
+
                 JOIN ap.prices pri
-                
+
                 JOIN ap.additionalProductCategory apc
-                
+
                 WHERE ap.enabled = TRUE
-                
+
                 AND pri.date < '2099.01.01'
-                
+
                 GROUP BY ap.identifier
-                
+
                 ORDER BY apc.positionInSubMenu , ap.positionInList
-                
+
                 ");
 
         $additionalproducts = $query->getResult();
 
 
         $query = $em->createQuery("
-                SELECT 
-                
+                SELECT
+
                     rt.identifier ,
                     MIN(av.quantity) AS quantity ,
                     MAX(pr.value) AS price
-                
+
                 FROM AppBundle:RoomType rt
-                
+
                 JOIN rt.availabilities av
-                
-                JOIN rt.prices pr 
-                
+
+                JOIN rt.prices pr
+
                 WHERE av.quantity > 0
-                
+
                 AND rt.enabled = TRUE
-                
-                AND av.date < '2099.01.01' 
-                
+
+                AND av.date < '2099.01.01'
+
                 AND pr.date < '2099.01.01'
-                
+
                 GROUP BY rt.identifier
-                
+
                 ORDER BY rt.positionInSubMenu
-                
+
                 ");
 
         $roomtypes = $query->getResult();
