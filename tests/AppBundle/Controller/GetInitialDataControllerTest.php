192675e7c17019e02d97a8eef513a063074a5a8d
Jan Helmle <jhelmle@gmx.de>
2016-09-19 16:58:42 +0200

    Tests : GetInitialDataControllerTest geaendert

--- tests/AppBundle/Controller/GetInitialDataControllerTest.php
+++ tests/AppBundle/Controller/GetInitialDataControllerTest.php
@@ -45,80 +45,80 @@ class GetInitialDataControllerTest extends WebTestCase {
         $crawler = $client->request('GET', '/api/v0/getinitialdata');
 
         $expected = '{
-	"roomtypes": [{
-		"identifier": "singleroom",
-		"subMenuText": "Einzel",
-		"listText": "Einzelzimmer",
-		"pricingBasisText": "/Nacht",
-		"capacity": 5
-	}, {
-		"identifier": "doubleroom",
-		"subMenuText": "Doppel",
-		"listText": "Doppelzimmer",
-		"pricingBasisText": "/Nacht",
-		"capacity": 6
-	}, {
-		"identifier": "twinroom",
-		"subMenuText": "Zweibett",
-		"listText": "Zweibettzimmer",
-		"pricingBasisText": "/Nacht",
-		"capacity": 7
-	}, {
-		"identifier": "tripleroom",
-		"subMenuText": "Dreibett",
-		"listText": "Dreibettzimmer",
-		"pricingBasisText": "/Nacht",
-		"capacity": 8
-	}, {
-		"identifier": "familyroom",
-		"subMenuText": "Familie",
-		"listText": "Familien-/Vierbettzimmer",
-		"pricingBasisText": "/Nacht",
-		"capacity": 4
-	}, {
-		"identifier": "apartmentsingle",
-		"subMenuText": "Apartm. EZ",
-		"listText": "Apartment als Einzelzimmer",
-		"pricingBasisText": "/Nacht",
-		"capacity": 3
-	}, {
-		"identifier": "apartmentdouble",
-		"subMenuText": "Apartm. DZ",
-		"listText": "Apartment als Doppelzimmer",
-		"pricingBasisText": "/Nacht",
-		"capacity": 2
-	}],
-	"boardings": [{
-		"identifier": "halfpension",
-		"listText": "Halbpension (mit Frühstück)",
-		"pricingBasisText": "/Pers. u. Nacht"
-	}, {
-		"identifier": "fullpension",
-		"listText": "Vollpension (3 Mahlzeiten)",
-		"pricingBasisText": "/Pers. u. Nacht"
-	}, {
-		"identifier": "breakfast",
-		"listText": "Nur Frühstück",
-		"pricingBasisText": "/Pers. u. Nacht"
-	}, {
-		"identifier": "noboarding",
-		"listText": "Ohne Verpflegung",
-		"pricingBasisText": ""
-	}],
-	"specials": [{
-		"identifier": "champagnebreakfast",
-		"listText": "Sektfrühstück",
-		"pricingBasisText": "/Pers. u. Nacht"
-	}, {
-		"identifier": "rosesinrooms",
-		"listText": "Rosen auf das Zimmer",
-		"pricingBasisText": ""
-	}, {
-		"identifier": "raftingtour",
-		"listText": "Rafting-Tour",
-		"pricingBasisText": "/Person"
-	}]
-        }';
+    "roomtypes": [{
+        "identifier": "singleroom",
+        "subMenuText": "Einzel",
+        "listText": "Einzelzimmer",
+        "pricingBasisText": "/Nacht",
+        "capacity": 11
+            }, {
+        "identifier": "doubleroom",
+        "subMenuText": "Doppel",
+        "listText": "Doppelzimmer",
+        "pricingBasisText": "/Nacht",
+        "capacity": 10
+            }, {
+        "identifier": "twinroom",
+        "subMenuText": "Zweibett",
+        "listText": "Zweibettzimmer",
+        "pricingBasisText": "/Nacht",
+        "capacity": 9
+            }, {
+        "identifier": "tripleroom",
+        "subMenuText": "Dreibett",
+        "listText": "Dreibettzimmer",
+        "pricingBasisText": "/Nacht",
+        "capacity": 8
+            }, {
+        "identifier": "familyroom",
+        "subMenuText": "Familie",
+        "listText": "Familien-/Vierbettzimmer",
+        "pricingBasisText": "/Nacht",
+        "capacity": 7
+            }, {
+        "identifier": "apartmentsingle",
+        "subMenuText": "Apartm. EZ",
+        "listText": "Apartment als Einzelzimmer",
+        "pricingBasisText": "/Nacht",
+        "capacity": 6
+            }, {
+        "identifier": "apartmentdouble",
+        "subMenuText": "Apartm. DZ",
+        "listText": "Apartment als Doppelzimmer",
+        "pricingBasisText": "/Nacht",
+        "capacity": 5
+            }],
+    "boardings": [{
+        "identifier": "halfpension",
+        "listText": "Halbpension (mit Frühstück)",
+        "pricingBasisText": "/Pers. u. Nacht"
+            }, {
+        "identifier": "fullpension",
+        "listText": "Vollpension (3 Mahlzeiten)",
+        "pricingBasisText": "/Pers. u. Nacht"
+            }, {
+        "identifier": "breakfast",
+        "listText": "Nur Frühstück",
+        "pricingBasisText": "/Pers. u. Nacht"
+            }, {
+        "identifier": "noboarding",
+        "listText": "Ohne Verpflegung",
+        "pricingBasisText": ""
+            }],
+    "specials": [{
+        "identifier": "champagnebreakfast",
+        "listText": "Sektfrühstück",
+        "pricingBasisText": "/Pers. u. Nacht"
+            }, {
+        "identifier": "rosesinrooms",
+        "listText": "Rosen auf das Zimmer",
+        "pricingBasisText": ""
+            }, {
+        "identifier": "raftingtour",
+        "listText": "Rafting-Tour",
+        "pricingBasisText": "/Person"
+            }]
+}';
 
         $this->assertJsonStringEqualsJsonString($client->getResponse()->getContent(), $expected);
     }
@@ -162,85 +162,85 @@ class GetInitialDataControllerTest extends WebTestCase {
         $crawler = $client->request('GET', '/api/v0.1/getinitialdata');
 
         $expected = '{
-	"roomTypes": {
-		"singleroom": {
-			"subMenuText": "Einzel",
-			"listText": "Einzelzimmer",
-			"pricingBasisText": "/Nacht",
-			"capacity": 5
-		},
-		"doubleroom": {
-			"subMenuText": "Doppel",
-			"listText": "Doppelzimmer",
-			"pricingBasisText": "/Nacht",
-			"capacity": 6
-		},
-		"twinroom": {
-			"subMenuText": "Zweibett",
-			"listText": "Zweibettzimmer",
-			"pricingBasisText": "/Nacht",
-			"capacity": 7
-		},
-		"tripleroom": {
-			"subMenuText": "Dreibett",
-			"listText": "Dreibettzimmer",
-			"pricingBasisText": "/Nacht",
-			"capacity": 8
-		},
-		"familyroom": {
-			"subMenuText": "Familie",
-			"listText": "Familien-/Vierbettzimmer",
-			"pricingBasisText": "/Nacht",
-			"capacity": 4
-		},
-		"apartmentsingle": {
-			"subMenuText": "Apartm. EZ",
-			"listText": "Apartment als Einzelzimmer",
-			"pricingBasisText": "/Nacht",
-			"capacity": 3
-		},
-		"apartmentdouble": {
-			"subMenuText": "Apartm. DZ",
-			"listText": "Apartment als Doppelzimmer",
-			"pricingBasisText": "/Nacht",
-			"capacity": 2
-		}
-	},
-	"boardings": {
-		"subMenuText": "Verpflegung",
-		"halfpension": {
-			"listText": "Halbpension (mit Frühstück)",
-			"pricingBasisText": "/Pers. u. Nacht"
-		},
-		"fullpension": {
-			"listText": "Vollpension (3 Mahlzeiten)",
-			"pricingBasisText": "/Pers. u. Nacht"
-		},
-		"breakfast": {
-			"listText": "Nur Frühstück",
-			"pricingBasisText": "/Pers. u. Nacht"
-		},
-		"noboarding": {
-			"listText": "Ohne Verpflegung",
-			"pricingBasisText": ""
-		}
-	},
-	"specials": {
-		"subMenuText": "Specials (optional)",
-		"champagnebreakfast": {
-			"listText": "Sektfrühstück",
-			"pricingBasisText": "/Pers. u. Nacht"
-		},
-		"rosesinrooms": {
-			"listText": "Rosen auf das Zimmer",
-			"pricingBasisText": ""
-		},
-		"raftingtour": {
-			"listText": "Rafting-Tour",
-			"pricingBasisText": "/Person"
-		}
-	}
-    }';
+  "roomTypes": {
+    "singleroom": {
+      "subMenuText": "Einzel",
+      "listText": "Einzelzimmer",
+      "pricingBasisText": "/Nacht",
+      "capacity": 11
+    },
+    "doubleroom": {
+      "subMenuText": "Doppel",
+      "listText": "Doppelzimmer",
+      "pricingBasisText": "/Nacht",
+      "capacity": 10
+    },
+    "twinroom": {
+      "subMenuText": "Zweibett",
+      "listText": "Zweibettzimmer",
+      "pricingBasisText": "/Nacht",
+      "capacity": 9
+    },
+    "tripleroom": {
+      "subMenuText": "Dreibett",
+      "listText": "Dreibettzimmer",
+      "pricingBasisText": "/Nacht",
+      "capacity": 8
+    },
+    "familyroom": {
+      "subMenuText": "Familie",
+      "listText": "Familien-/Vierbettzimmer",
+      "pricingBasisText": "/Nacht",
+      "capacity": 7
+    },
+    "apartmentsingle": {
+      "subMenuText": "Apartm. EZ",
+      "listText": "Apartment als Einzelzimmer",
+      "pricingBasisText": "/Nacht",
+      "capacity": 6
+    },
+    "apartmentdouble": {
+      "subMenuText": "Apartm. DZ",
+      "listText": "Apartment als Doppelzimmer",
+      "pricingBasisText": "/Nacht",
+      "capacity": 5
+    }
+  },
+  "boardings": {
+    "subMenuText": "Verpflegung",
+    "halfpension": {
+      "listText": "Halbpension (mit Frühstück)",
+      "pricingBasisText": "/Pers. u. Nacht"
+    },
+    "fullpension": {
+      "listText": "Vollpension (3 Mahlzeiten)",
+      "pricingBasisText": "/Pers. u. Nacht"
+    },
+    "breakfast": {
+      "listText": "Nur Frühstück",
+      "pricingBasisText": "/Pers. u. Nacht"
+    },
+    "noboarding": {
+      "listText": "Ohne Verpflegung",
+      "pricingBasisText": ""
+    }
+  },
+  "specials": {
+    "subMenuText": "Specials (optional)",
+    "champagnebreakfast": {
+      "listText": "Sektfrühstück",
+      "pricingBasisText": "/Pers. u. Nacht"
+    },
+    "rosesinrooms": {
+      "listText": "Rosen auf das Zimmer",
+      "pricingBasisText": ""
+    },
+    "raftingtour": {
+      "listText": "Rafting-Tour",
+      "pricingBasisText": "/Person"
+    }
+  }
+}';
 
         $this->assertJsonStringEqualsJsonString($client->getResponse()->getContent(), $expected);
     }
